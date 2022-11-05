d2a1fc062163f7ea6396d0564863437493af57d0
Eugene <zhekul.90@gmail.com>
2022-11-05 12:48:18 +0300

    Add Coordinates DAL model

---
+++ Q10.Pickpoint.Models/DAL/Coordinates.cs
@@ -0,0 +1,9 @@
+﻿namespace Q10.Pickpoint.Models;
+
+public class Coordinates
+{
+    public string GlobalId { get; set; }
+    public float Float0 { get; set; }
+    public float Float1 { get; set; }
+    public int? District { get; set; }
+}
