a4ba4da89c128b6da48b5df03ff15756f1432206
Eugene <zhekul.90@gmail.com>
2022-11-06 18:35:45 +0300

    Correct model

--- Q10.Pickpoint.Models/DAL/Coordinates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Q10.Pickpoint.Models;
-
-public class Coordinates
-{
-    public string GlobalId { get; set; }
-    public string Float0 { get; set; }
-    public string Float1 { get; set; }
-}
---
+++ Q10.Pickpoint.Models/Service/Coordinates.cs
@@ -0,0 +1,9 @@
+﻿namespace Q10.Pickpoint.Models;
+
+public class Coordinates
+{
+    public int Id { get; set; }
+    public string GlobalId { get; set; }
+    public double Float_0 { get; set; }
+    public double Float_1 { get; set; }
+}
